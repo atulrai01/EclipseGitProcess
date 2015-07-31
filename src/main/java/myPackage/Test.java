bd274d202c4d6e389bc661f57f0a62e77b07c2d6
Atul Rai <Atul Rai@192.168.8.103>
2015-07-31 15:35:24 +0530

    add int a, b

--- src/main/java/myPackage/Test.java
+++ src/main/java/myPackage/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Eclipse Git Integration with Concretepage.com");
+		
+		int a= 5, b= 10;
+		int c = a+b;
+		
 	}
 
 }
