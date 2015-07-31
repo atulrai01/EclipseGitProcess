cac8720bd8c92591f8672559ef4bd043ec3a08c4
Atul Rai <Atul Rai@192.168.8.103>
2015-07-31 15:34:04 +0530

    add welcome

--- src/main/java/myPackage/Test.java
+++ src/main/java/myPackage/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Eclipse Git Integration with Concretepage.com");
+		System.out.println("Welcome to Eclipse Git Integration with Concretepage.com");
 	}
 
 }
